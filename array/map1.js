48885784b37031100ea13c14df5c271de785a6e4
Vandilson <vandilson25@outlook.com>
2022-09-09 19:36:05 -0300

    commit inicial

--- array/map1.js
+++ array/map1.js
@@ -1,15 +1,15 @@
 const nums = [1, 2, 3, 4, 5]
 
 // For com propósito // o map gera um novo array
-let resultado = nums.map(function(e){
-    return e * 2
+let resultado = nums.map(function(elemento){
+    return elemento * 2
 })
 
 console.log(resultado)
 
-const soma10 = e => e + 10
-const triplo = e => e * 3
-const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+const soma10 = elemento => elemento + 10
+const triplo = elemento => elemento * 3
+const paraDinheiro = elemento => `R$ ${parseFloat(elemento).toFixed(2).replace('.', ',')}`
 
 resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
 console.log(resultado)
